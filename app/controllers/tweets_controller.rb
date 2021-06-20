b975eda97f26058218573cabc86e3fc4cf341011
AstinCHOI <asciineo@gmail.com>
2021-06-21 00:11:01 +0900

    add sleep for slow query example

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,8 @@ class TweetsController < ApplicationController
   def index
     response = HTTParty.get('http://twitter.com/newrelic')
     
+    icon = Icon.find_by_sql("select pg_sleep(5)")
+
     parsed_data = Nokogiri::HTML.parse response.body
     tweetNodes = parsed_data.css(".js-tweet-text")
     @nodes = tweetNodes.collect do |node|
